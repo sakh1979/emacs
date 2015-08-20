906b377083ba49f300368a17f3a84377f27bebe8
Sagun Khatri <skhatri@homeaway.com>
2015-08-20 11:48:35 -0500

    Adding spinner package from el-get

--- .emacs.d/settings/el-get-settings.el
+++ .emacs.d/settings/el-get-settings.el
@@ -30,6 +30,7 @@
                projectile
                yaml-mode
                nyan-mode
+               spinner
                ))
 
 ; first enable shallow clone, so we don't need to clone the entire
